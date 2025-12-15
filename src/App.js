78412015065db022dc89193fc2840be1598c0349
Sogand <sogandmohamadipour7@gmail.com>
2025-12-15 22:07:29 +0330

    memoize the Archive component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // memo as main logic of this version
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -20,10 +20,10 @@ function App() {
   const searchedPosts =
     searchQuery.length > 0
       ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
+        `${post.title} ${post.body}`
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase())
+      )
       : posts;
 
   function handleAddPost(post) {
@@ -58,7 +58,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={true} />
+      <Archive show={false} />
       <Footer />
     </section>
   );
@@ -155,8 +155,7 @@ function List({ posts }) {
   );
 }
 
-function Archive({ show }) {
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+const Archive = memo(function Archive({ show }) {
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
@@ -185,7 +184,7 @@ function Archive({ show }) {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
